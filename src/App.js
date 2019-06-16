9492dc07792f7b67c5c1afd73eb852e7bdad242a
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-06-16 18:32:29 -0500

    test add function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+export const add = (x, y) => x + y;
+
 function App() {
   return (
     <div className="App">
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { add } from "./App";
+
+test('add', () => {
+    expect(add(1,2)).toBe(3);
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
