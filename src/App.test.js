afc9d45df12bdf23441c54c7e489672a56b7115a
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-06-25 15:33:47 -0500

    mock a function for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { add, total } from "./App";
+// import { add, total } from "./App";
 
+const add = jest.fn(()=>3)
 // unit test
 test('add', () => {
-    expect(add(1,2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
+  expect(add).toHaveBeenCalledTimes(1);
+  expect(add).toHaveBeenCalledWith(1, 2);
 })
 
 //integration test
-test('total', () => {
-  expect(total(5, 20)).toBe('$25');
-})
+// test('total', () => {
+//   expect(total(5, 20)).toBe('$25');
+// })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
